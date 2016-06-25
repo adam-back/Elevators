96145d8bfc8ac151efb9ad2a89ee7a8e5d55ef0b
Adam Back <adb33@email.arizona.edu>
2016-06-25 13:09:02 -0700

    (feat) Added some intial functionality to the elevator class.

--- elevator.js
+++ elevator.js
@@ -1,1 +1,19 @@
 // elevator class
+
+var Elevator = function() {
+  // start at ground floor
+  this.currentFloor = 1;
+  this.moveToFloor = function( destinationFloor ) {
+    while( this.currentFloor !== destinationFloor ) {
+      if ( this.currentFloor < destinationFloor ) {
+        this.currentFloor++;
+      } else {
+        this.currentFloor--;
+      }
+
+      console.log( 'Moved to floor ', this.currentFloor );
+    }
+
+    console.log( 'At destination. Opening doors.' );
+  }
+};
--- initialize.js
+++ initialize.js
@@ -1,7 +1,6 @@
-
 var createElevatorSim = function( numberOfFloors, elevators ) {
   if ( numberOfFloors < 1 || elevators < 1 ) {
     throw new Error( 'Minimum of one floor and elevator needed!' );
   }
 
-}
+};
