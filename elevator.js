5570ddb692b69c46e572528bf00f4931e4ef4d29
Adam Back <adb33@email.arizona.edu>
2016-06-25 13:56:01 -0700

    (feat) Added some movement helper classes to elevator.

--- elevator.js
+++ elevator.js
@@ -3,8 +3,17 @@
 var Elevator = function() {
   // start at ground floor
   this.currentFloor = 1;
-  this.destination = null;
+  this.destinations = [];
   this.moving = false;
+  this.history = {
+    floors: 0,
+    trips: 0
+  };
+  this.maintenanceMode = false;
+  this.openDoor = function() {
+    console.log( 'Opening door to pick up passenger.' );
+  }
+
   this.moveToFloor = function( destinationFloor ) {
     this.destination = destinationFloor;
     this.moving = true;
@@ -23,3 +32,27 @@ var Elevator = function() {
     this.moving = false;
   }
 };
+
+Elevator.prototype.startTrip = function( pickupFloor, destinationFloor ) {
+  this.destinations.push( { pickup: pickupFloor, destination: destinationFloor, hasPickedUpPassenger: false } );
+  this.history.trips++;
+  this.moving = true;
+};
+
+Elevator.prototype.move = function() {
+  if ( this.moving === true ) {
+    var nextDestination = destinations[ 0 ];
+    if ( nextDestination.hasPickedUpPassenger ) {
+      // move towards destination
+    } else {
+      // move towards passenger call
+    }
+
+    this.history.floors++;
+    console.log( 'Moving to floor' );
+  // elevator is unoccupied and has no destinations
+  } else {
+    return;
+  }
+};
+
