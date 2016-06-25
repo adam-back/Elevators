d7b49ed803e015f958f87116b3abbc920467be23
Adam Back <adb33@email.arizona.edu>
2016-06-25 13:03:50 -0700

    (feat) Created fn to initialize simulation.

---
+++ initialize.js
@@ -0,0 +1,7 @@
+
+var createElevatorSim = function( numberOfFloors, elevators ) {
+  if ( numberOfFloors < 1 || elevators < 1 ) {
+    throw new Error( 'Minimum of one floor and elevator needed!' );
+  }
+
+}
