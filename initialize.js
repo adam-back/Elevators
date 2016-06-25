ea4af32beaee13764f3c273c02bcef5c917b41e1
Adam Back <adb33@email.arizona.edu>
2016-06-25 13:30:20 -0700

    (feat) Finished basis of initialize fn.

--- initialize.js
+++ initialize.js
@@ -1,7 +1,23 @@
+var elevators = [];
+
 var createElevatorSim = function( numberOfFloors, numberOfElevators ) {
+  // enforce minimums
   if ( numberOfFloors < 1 || numberOfElevators < 1 ) {
     throw new Error( 'Minimum of one floor and elevator needed!' );
   }
 
-  return elevators;
+  // create
+  for ( var i = 0; i < numberOfElevators.length; i++ ) {
+    // has access to Elevator class
+    elevators.push( new Elevator() );
+  }
+
+  // return interval id for step-by-step movement of elevators
+  return setInterval(function() {
+    // in order to conceptualize movement, I've chosen to step elevators once per seconds
+    handleNewPassengerRequests();
+    moveElevatorsOneFloor();
+  }, 1000);
 };
+
+// createElevatorSim( 4, 2 );
